1e8cc08111cb678c9126629eeb1b525a56221e78
Robert Pergl <robert@pergl.org>
2019-11-25 21:31:20 +0100

    remove ignored files in dist/

--- dist/shared/server.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.endpointUrl = "https://b2note.bsc.es";
-exports.apiUrl = "/api";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2VydmVyLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vYXBwL3NoYXJlZC9zZXJ2ZXIudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBYSxRQUFBLFdBQVcsR0FBRyx1QkFBdUIsQ0FBQztBQUV0QyxRQUFBLE1BQU0sR0FBRyxNQUFNLENBQUMifQ==
