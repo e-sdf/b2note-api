4167147eaef5d74e3778162dd4810f7373a70ff7
Robert Pergl <robert@pergl.org>
2019-12-22 17:36:52 +0100

    addCore script

--- scripts/addCore.sh
+++ scripts/addCore.sh
@@ -3,5 +3,5 @@
 # Add b2note-core git subtree
 
 echo "Adding b2note-core as a git subtree to app"
-git remote add –f core git@github.com:roper79/b2note-core.git
+git remote add core git@github.com:roper79/b2note-core.git
 git subtree add --prefix=app/core core master --squash
