977d71b5f063e5571e2336317b3588850cb065db
Robert Pergl <robert@pergl.org>
2020-03-19 12:30:08 +0100

    preparation for GAUTH

--- app/config.ts
+++ app/config.ts
@@ -6,27 +6,37 @@ interface Config {
   b2notePort: string;
   mongodbUrl: string;
   sessionSecret: string;
-  b2accessUrl: string;
-  b2accessConfigurationPath: string;
+  b2access: boolean;
+  b2accessConfigurationUrl: string;
   b2accessClientId: string;
   b2accessClientSecret: string;
   b2accessRedirectUrl: string;
+  gAuth: boolean;
+  gAuthConfigurationUrl: string;
+  gAuthClientId: string;
+  gAuthClientSecret: string;
+  gAuthRedirectUrl: string;
 };
 
 const globalConfig: Config = {
   b2notePort: process.env.B2NOTE_PORT || "",
   mongodbUrl: process.env.MONGODB_URL || "",
   sessionSecret: process.env.SESSION_SECRET || "",
-  b2accessUrl: process.env.B2ACCESS_URL  || "",
-  b2accessConfigurationPath: process.env.B2ACCESS_CONFIGURATION_PATH || "",
+  b2access: process.env.B2ACCESS == "true",
+  b2accessConfigurationUrl: process.env.B2ACCESS_CONFIGURATION_URL || "",
   b2accessClientId: process.env.B2ACCESS_CLIENT_ID || "",
   b2accessClientSecret: process.env.B2ACCESS_CLIENT_SECRET || "",
   b2accessRedirectUrl: process.env.B2ACCESS_REDIRECT_URL || "",
+  gAuth: process.env.GAUTH == "true",
+  gAuthConfigurationUrl: process.env.GAUTH_CONFIGURATION_URL || "",
+  gAuthClientId: process.env.GAUTH_CLIENT_ID || "",
+  gAuthClientSecret: process.env.GAUTH_CLIENT_SECRET || "",
+  gAuthRedirectUrl: process.env.GAUTH_REDIRECT_URL || "",
 };
 
 // Check if all env variables were present
 const configValues = Object.values(globalConfig);
-const nonEmpty = configValues.filter(v => v.length > 0);
+const nonEmpty = configValues.filter(v => v || v.length > 0);
 if (configValues.length !== nonEmpty.length) {
   logError("Missing some env variable(s):");
   logError(JSON.stringify(globalConfig, null, 2));
--- app/server.ts
+++ app/server.ts
@@ -1,10 +1,10 @@
-import * as http  from "http";
+import * as http from "http";
 import { Request, Response } from "express";
 import passport from "passport";
 import config from "./config";
 import app from "./app";
 import { logError } from "./logging";
-import { Issuer, Strategy as OpenIdStrategy, TokenSet, UserinfoResponse } from "openid-client";
+import { custom, Issuer, Strategy as OpenIdStrategy, TokenSet, UserinfoResponse, ClientMetadata } from "openid-client";
 import { Strategy as BearerStrategy } from "passport-http-bearer";
 import { User } from "./core/user";
 import * as responses from "./responses";
@@ -15,21 +15,52 @@ const b2accessSessionKey = "b2access";
 
 config.dumpConfig();
 
+custom.setHttpOptionsDefaults({
+  timeout: 10000,
+});
+
+// Setup auth configs
+// const authConfig: AuthConfig|null = matchSwitch(config.authProvider, {
+//   [AuthProvider.NONE]: () => { throw new Error("Auth provider not set"); return null; },
+//   [AuthProvider.B2ACCESS]: () => ({
+//     configurationUrl: config.b2accessConfigurationUrl,
+//     client_id: config.b2accessClientId,
+//     client_secret: config.b2accessClientSecret,
+//     redirect_uris: [config.b2accessRedirectUrl],
+//     response_types: ["code"]
+//   }),
+//   [AuthProvider.GAUTH]: () => ({
+//     configurationUrl: config.gAuthConfigurationUrl,
+//     client_id: config.gAuthClientId,
+//     client_secret: config.gAuthClientSecret,
+//     redirect_uris: [config.gAuthRedirectUrl],
+//     response_types: ["code"],
+//   })
+// });
+
+// const b2AccessInfoPromise = () => 
+//   config.b2access ? Issuer.discover(config.b2accessConfigurationUrl) : Promise.reject();
+
+// const gAuthInfoPromise = () => 
+//   config.gAuth ? Issuer.discover(config.gAuthConfigurationUrl) : Promise.reject();
+
 // Initialise B2ACCESS auth
 
-Issuer.discover(config.b2accessUrl + config.b2accessConfigurationPath).then(b2accessInfo => {
+console.log("Sending B2ACCESS OIDC Configuration Request to: ");
+console.log(config.b2accessConfigurationUrl);
+Issuer.discover(config.b2accessConfigurationUrl).then(authInfo => {
   console.log("Received B2ACCESS OIDC Configuration Response");
   app.use(passport.initialize());
   app.use(passport.session());
-  const b2accessClient = new b2accessInfo.Client({
+  const authClient = new authInfo.Client({
     client_id: config.b2accessClientId,
     client_secret: config.b2accessClientSecret,
     redirect_uris: [config.b2accessRedirectUrl],
-    response_types: ["code"],
+    response_types: ["code"]
   });
   const strategy = new OpenIdStrategy(
     {
-      client: b2accessClient,
+      client: authClient,
       sessionKey: b2accessSessionKey,
       params: { scope: "openid profile email"}
     },
@@ -64,20 +95,24 @@ Issuer.discover(config.b2accessUrl + config.b2accessConfigurationPath).then(b2ac
   
   passport.use("b2access", strategy);
 
-  app.get("/api/login", (req: Request, resp: Response) => {
+  app.get("/api/b2access/login", (req: Request, resp: Response) => {
     passport.authenticate("b2access", { session: false })(req, resp);
   });
 
-  app.get("/api/auth_callback", (req: Request, resp: Response) => {
+  // app.get("/api/gauth/login", (req: Request, resp: Response) => {
+  //   passport.authenticate("auth", { session: false })(req, resp);
+  // });
+
+  app.get("/api/b2access/auth_callback", (req: Request, resp: Response) => {
     // As B2ACCESS does not support cookies, we must retrieve the session info manually from DB
     popB2AccessSession(req.query.state)
-    .then(b2accessRecord => {
+    .then(authRecord => {
       if (req.session) {
-        req.session[b2accessSessionKey] = b2accessRecord;
+        req.session[b2accessSessionKey] = authRecord;
         passport.authenticate("b2access")(req, resp, (err: any) => {
           if (err) {
             logError(err);
-            responses.serverErr(resp, "Error processing auth callback");
+            responses.serverErr(resp, "Error processing B2ACCESS callback");
           } else {
             if (req.user) {
               responses.windowWithMessage(resp, JSON.stringify(req.user));
@@ -88,7 +123,7 @@ Issuer.discover(config.b2accessUrl + config.b2accessConfigurationPath).then(b2ac
         });
       } else { logError("Session support missing"); }
     })
-    .catch(err => logError("Failed retrieving b2access session from DB: " + err));
+    .catch(err => logError("Failed retrieving B2ACCESS session from DB: " + err));
   });
 
   passport.use(new BearerStrategy(
