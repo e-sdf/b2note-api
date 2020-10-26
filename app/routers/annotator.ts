8fff15053cd2168ac8335cf7c2caca76f16dc3f6
Robert Pergl <robert@pergl.org>
2020-10-26 17:42:23 +0100

    fix semicolons; fix missing cookie error

--- app/routers/annotator.ts
+++ app/routers/annotator.ts
@@ -1,48 +1,48 @@
-import { Request, Response, Router } from 'express'
-import axios from 'axios'
-import { processReponse } from '../annotator/content'
-import _ from 'lodash'
-import config from '../config'
+import { Request, Response, Router } from "express";
+import axios from "axios";
+import { processReponse } from "../annotator/content";
+import _ from "lodash";
+import config from "../config";
 
-const router = Router()
+const router = Router();
 
-const annotatorPath = '/annotator'
-const proxyUrl = `${config.serverPath}${annotatorPath}`
+const annotatorPath = "/annotator";
+const proxyUrl = `${config.serverPath}${annotatorPath}`;
 
 router.get(annotatorPath, (req: Request, res: Response) => {
-  const url = req.query.url as string
+  const url = req.query.url as string;
 
   if (_.isEmpty(url)) {
-    res.status(400).send()
+    res.status(400).send();
   } else {
-    proxyRequest(url, req, res)
+    proxyRequest(url, req, res);
   }
-})
+});
 
 
 function proxyRequest(url: string, req: Request, res: Response) {
   axios
     .get(url, {
-      responseType: 'arraybuffer',
+      responseType: "arraybuffer",
       headers: {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'accept-language': 'en-GB,en-US',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36',
-        'cookie': req.headers.cookie
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-language": "en-GB,en-US",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36",
+        ...(req.headers.cookie ? {"cookie": req.headers.cookie} : {})
       }
     })
     .then(response => {
-      res.setHeader('content-type', response.headers['content-type'])
-      res.send(processReponse(proxyUrl, url, response))
+      res.setHeader("content-type", response.headers["content-type"]);
+      res.send(processReponse(proxyUrl, url, response));
     })
     .catch(error => {
+      console.log(error);
       if (error.response) {
-        res.status(error.response.status).send()
+        res.status(error.response.status).send();
       } else {
-        res.status(500).send()
+        res.status(500).send();
       }
-    })
+    });
 }
 
-
-export default router
+export default router;
