ec2f32fcd450975c446561cc21ec7e9f8a62c3ed
Jan Slifka <jan.slifka@gmail.com>
2020-10-29 17:23:55 +0100

    Add annotator error

--- app/routers/annotator.ts
+++ app/routers/annotator.ts
@@ -28,7 +28,7 @@ function proxyRequest(url: string, req: Request, res: Response) {
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
         "accept-language": "en-GB,en-US",
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.80 Safari/537.36",
-        ...(req.headers.cookie ? {"cookie": req.headers.cookie} : {})
+        ...(req.headers.cookie ? { "cookie": req.headers.cookie } : {})
       }
     })
     .then(response => {
@@ -36,13 +36,16 @@ function proxyRequest(url: string, req: Request, res: Response) {
       res.send(processResponse(proxyUrl, url, response));
     })
     .catch(error => {
-      console.log(error);
-      if (error.response) {
-        res.status(error.response.status).send();
-      } else {
-        res.status(500).send();
-      }
+      console.log(error.toString());
+      const status = _.get(error, "response.status", 500);
+      res.status(status).send(createErrorPage(error.toString()));
     });
+}
+
+
+function createErrorPage(error: string): string {
+  const script = `parent.postMessage({ "type": "iframe.error", "error": "${error}" }, "*")`;
+  return `<html><body><script>${script}</script></body></html>`;
 }
 
 export default router;
