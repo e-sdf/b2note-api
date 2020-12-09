b626ebcc54903adde9800baa3f04690ae61c37c7
Jan Slifka <jan.slifka@gmail.com>
2020-12-09 10:50:31 +0100

    Add xls and csv annotation

--- app/annotator/content.ts
+++ app/annotator/content.ts
@@ -8,7 +8,7 @@ const srcRegex = /src=['"]((?:[^"'\/]*\/)*([^'"]+))['"]/g;
 const srcsetRegex = /srcset=['"]([^'"]+)['"]/g;
 const urlRegex = /url\(([^)]+)\)/g;
 const targetBlankRegex = /target=['"]_blank['"]/g;
-
+const tableMimeTypes = ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "application/vnd.ms-excel", "text/csv"];
 
 export function processResponse<A>(proxyUrl: string, baseUrl: string, root: boolean, response: AxiosResponse<A>): { contentType: string, data: string | A } {
   const contentType = response.headers["content-type"];
@@ -31,6 +31,13 @@ export function processResponse<A>(proxyUrl: string, baseUrl: string, root: bool
     return {
       contentType: "text/html",
       data: processRootImageContent(proxyUrl, baseUrl)
+    };
+  }
+
+  if (root && tableMimeTypes.some(type => _.includes(contentType, type))) {
+    return {
+      contentType: "text/html",
+      data: processTableContent(proxyUrl, baseUrl)
     };
   }
 
@@ -76,12 +83,16 @@ function processRootImageContent(proxyUrl: string, baseUrl: string) {
   return `<!doctype html><html><head>${headerExtra(proxyUrl, baseUrl, false)}</head><body><img src="${imageUrl}"></body></html>`;
 }
 
+function processTableContent(proxyUrl: string, baseUrl: string) {
+  return `<!doctype html><html><head>${headerExtraTable(proxyUrl, baseUrl)}</head><body><div id="table-wrapper" class="table-wrapper"></div><div id="sheetlist" class="sheetlist"></div></body></html>`;
+}
+
 function staticScript(script: string, defer = false) {
   return `<script src="${config.serverPath}/static/dist/${script}.js" ${defer ? "defer" : ""}></script>`;
 }
 
 function styleLink(style: string) {
-  return `<link rel="stylesheet" type="text/css" href="${config.serverPath}/static/dist/${style}.css" />`;
+  return `<link rel="stylesheet" type="text/css" href="${config.serverPath}/static/dist/css/${style}.css" />`;
 }
 
 
@@ -122,10 +133,19 @@ function replaceCssUrl(proxyUrl: string, baseUrl: string, url: string): string {
 
 // Helpers
 
-function headerExtra(proxyUrl: string, baseUrl: string, annotatePage = true) {
+function headerExtra(proxyUrl: string, baseUrl: string, annotatePage = true): string {
   const baseUrlScript = `<script>window.baseUrl = '${baseUrl}'; window.proxyUrl = '${proxyUrl}'; window.annotatePage = ${annotatePage};</script>`;
   const overridesScript = staticScript("annotator-overrides");
   const iframeScript = staticScript("annotator-iframe", true);
-  const style = styleLink("main");
+  const style = styleLink("annotator-iframe");
   return `${baseUrlScript}${overridesScript}${iframeScript}${style}`;
+}
+
+function headerExtraTable(proxyUrl: string, baseUrl: string): string {
+  const tableUrl = replaceUrl(proxyUrl, baseUrl, baseUrl);
+  const baseUrlScript = `<script>window.tableUrl = '${tableUrl}';</script>`;
+  const script = staticScript("annotator-iframe-table");
+  const baseStyle = styleLink("annotator-iframe");
+  const tableStyle = styleLink("annotator-iframe-table");
+  return `${baseUrlScript}${script}${tableStyle}${baseStyle}`;
 }
